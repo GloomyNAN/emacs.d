e0a245ae6df417fc08217c653fed1e2f2962f178
Gloomy楠 <GloomyNAN@163.com>
2023-03-08 23:35:36 +0800

    update osx settings

--- lisp/init-kbd.el
+++ lisp/init-kbd.el
@@ -2,23 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(defconst *is-mac* (eq system-type 'darwin)
-  "Const for system check, macOS.")
-
-(defconst *is-linux* (eq system-type 'gnu/linux)
-  "Const for system check, GNU/Linux.")
-
-(defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
-  "Const for system check, Windows or DOS.")
-
-;; Settings for macOS key: Use command as the Meta key
-(when *is-mac*
-  (setq mac-right-command-modifier 'meta)
-  (setq mac-right-option-modifier 'super))
-
-
 (fset 'yes-or-no-p 'y-or-n-p);reset yes/no to y/n
-
 
 (provide 'init-kbd)
 ;;; init-kbd ends here
---
+++ lisp/init-os.el
@@ -0,0 +1,28 @@
+;;; init-os.el ---  Consts operating system
+;;; Commentary:
+;;; Code:
+
+(defconst *is-mac* (eq system-type 'darwin)
+  "Const for system check, macOS.")
+
+(defconst *is-linux* (or (eq system-type 'gnu) (eq system-type 'gnu/linux) (eq system-type 'gnu/kfreebsd))
+  "Const for system check, GNU/Linux.")
+
+(defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt))
+  "Const for system check, Windows or DOS.")
+
+(when *is-mac*
+  
+  ;; Settings for macOS key: Use command as the Meta key
+  (setq mac-right-command-modifier 'meta)
+  (setq mac-right-option-modifier 'super)
+
+  (use-package reveal-in-osx-finder
+    :bind ("C-c f" . 'reveal-in-osx-finder))
+  (use-package osx-dictionary
+    :bind("C-c d" . 'osx-dictionary-search-word-at-point))
+  (use-package counsel-osx-app
+    :bind("C-c /" . 'counsel-osx-app)))
+
+(provide 'init-os)
+;;; init-kbd ends here
