a3a7564b5a88206988087dd5583e16b82e970b2f
GloomyNAN <gloomynan@gmail.com>
2023-03-10 18:15:46 +0800

    add dashboard

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,11 +2,38 @@
 ;;; Commentary:
 ;;; code:
 
+;; Github https://github.com/emacs-dashboard/
+(use-package dashboard
+  :config
+  (dashboard-setup-startup-hook)
+  :init
+  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*"))
+        dashboard-banner-logo-title "Welcome to the new life!"
+        dashboard-startup-banner (expand-file-name "asstes/banner.txt" user-emacs-directory)
+        dashboard-center-content t
+        dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5))
+        dashboard-set-heading-icons t
+        dashboard-set-file-icons t
+        dashboard-modify-heading-icons '((recents . "file-text")
+                                       (bookmarks . "book"))
+        dashboard-set-navigator t
+
+        dashboard-set-init-info t
+        dashboard-init-info "Are you ready?"
+        dashboard-projects-switch-function 'projectile-persp-switch-project
+        dashboard-week-agenda t
+        dashboard-filter-agenda-entry 'dashboard-no-filter-agenda)
+  (add-to-list 'dashboard-items '(agenda) t)
+  (dashboard-open))
+
 ;;; themes
 (use-package monokai-theme)
 (use-package dracula-theme)
 (use-package doom-themes)
-
 
 ;;(load-theme 'monokai t)
 (load-theme 'dracula t)
@@ -55,6 +82,10 @@
 
 (use-package vscode-icon
   :commands (vscode-icon-for-file))
+
+;; Github https://github.com/domtronn/all-the-icons.el
+(use-package all-the-icons
+  :if (display-graphic-p))
 
 (provide 'init-themes)
 ;;; init-themes ends here
