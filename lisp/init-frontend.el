2666c3972a385c1d60f02372852eafd4ac6a32c8
GloomyNAN <gloomynan@gmail.com>
2023-03-13 15:41:55 +0800

    fix web-beatutify

--- lisp/init-frontend.el
+++ lisp/init-frontend.el
@@ -37,14 +37,20 @@
 ;;; Github https://github.com/yasuyk/web-beautify
 ;;; run command 'npm -g install js-beautify'
 (use-package web-beautify
-  :bind
-  (:map web-mode-map ("C-c b" . 'web-beautify-html))
-  (:map css-mode-map ("C-c b" . 'web-beautify-html))
-  (:map js-mode-map ("C-c b" . 'web-beautify-html))
-  (:map js2-mode-map ("C-c b" . 'web-beautify-html))
-  (:map html-mode-map ("C-c b" . 'web-beautify-html))
-  (:map json-mode-map ("C-c b" . 'web-beautify-html))
-  )
+  :init
+  (eval-after-load 'js2-mode
+    '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+  ;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
+  (eval-after-load 'js
+  '(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
+  (eval-after-load 'json-mode
+    '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+  (eval-after-load 'sgml-mode
+    '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+  (eval-after-load 'web-mode
+    '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
+  (eval-after-load 'css-mode
+    '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css)))
 
 ;;; Github https://github.com/AdamNiederer/vue-mode/
 (use-package vue-mode)
